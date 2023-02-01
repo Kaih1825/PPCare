b34222a6f31a80ee21139ca443a392809b6739cc
Kai <imatch11@gmail.com>
2023-02-01 16:05:07 +0800

    0201 新增專案

--- app/src/main/java/com/example/ppcare/Activitys/login.kt
+++ app/src/main/java/com/example/ppcare/Activitys/login.kt
@@ -1,11 +1,12 @@
-package com.example.ppcare
+package com.example.ppcare.Activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ppcare.R
 
-class MainActivity : AppCompatActivity() {
+class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
